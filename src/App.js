fe0417ecf3d99089b6af5ebef18b25222a9509dd
TheIllusiveC4 <gasingdong@gmail.com>
2019-07-22 12:13:14 -0700

    implement routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 import './App.css';
 import { Home, About, Contact, Navigation } from './components';
 
 const App = () => (
   <div>
+    <Route path="/" exact component={Home} />
+    <Route path="/about" exact component={About} />
+    <Route path="/contact" exact component={Contact} />
     <Navigation />
   </div>
 );
